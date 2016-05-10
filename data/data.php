f520f734f28d33060e897620101214da9cb88c32
juhelmarc <juhelmarc@gmail.com>
2016-05-10 07:52:52 -0400

    ajout  array

--- data/data.php
+++ data/data.php
@@ -1,32 +1,135 @@
 <?php
-/**
- * Tableau principal des données du site
- * Les clefs des items sont leurs "id" (un numéro)
- * (suivant le projet) Chaque item doit contenir au minimum les champs suivants
- * - nom
- * - categorie
- * - etc
- */
-$data = array(
-    0 => array(
-        'nom' => 'Trekking',
-        'categorie' => 'aventure',
-        'prix' => 1899.99,
-        'photo' => 'toto.jpg',
-        'dans_carousel' => true,
+
+$sports = array(
+    01 => array(
+        'nom' => 'Fifa 2016',
+        'catégorie' => 'sports',
+        'prix' => 39.99,
+        'photo' => 'fifa.jpg'
     ),
-    1 => array(
-        'nom' => 'Robots',
-        'categorie' => 'science-fiction',
-        'prix' => 489.99,
-        'photo' => 'kiki.jpg',
-        'dans_carousel' => false,
+    02 => array(
+        'nom' => 'NBA 2k 16',
+        'catégorie' => 'sports',
+        'prix' => 54.99,
+        'photo' => 'nba.jpg'
     ),
-    2 => array(
-        'nom' => 'Football',
-        'categorie' => 'sport',
-        'prix' => 88.00,
-        'photo' => 'foot.jpg',
-        'dans_carousel' => true,
+    03 => array(
+        'nom' => 'Madden NFL 2016',
+        'catégorie' => 'nfl.jpg',
+        'prix' => 49.99,
+        'photo' => 'photo'
+    ),
+    04 => array(
+        'nom' => 'UFC',
+        'catégorie' => 'sports',
+        'prix' => 34.99,
+        'photo' => 'ufc.jpg'
+    ),
+    05 => array(
+        'nom' => 'Infinity Air',
+        'catégorie' => 'sports',
+        'prix' => 'coming soon',
+        'photo' => 'infinite.jpeg'  // attention à l'extension ici
     ),
 );
+
+$jeunes = array(
+    01 => array(
+        'nom' => 'Tortues Ninja',
+        'catégorie' => 'jeunesse',
+        'prix' => 49.99,
+        'photo' => 'TNMT.jpg'
+    ),
+    02 => array(
+        'nom' => 'Mystery Castle',
+        'catégorie' => 'jeunesse',
+        'prix' => 9.99,
+        'photo' => 'mystery.png'
+    ),
+    03 => array(
+        'nom' => 'JumpJet Rex',
+        'catégorie' => 'jeunesse',
+        'prix' => 9.99,
+        'photo' => 'jumpjet.png'
+    ),
+    04 => array(
+        'nom' => 'Gryphon Knight Epic',
+        'catégorie' => 'jeunesse',
+        'prix' => 12.99,
+        'photo' => 'gryphon.png'
+    ),
+    05 => array(
+        'nom' => 'Ori ans the Blind Forest',
+        'catégorie' => 'jeunesse',
+        'prix' => 14.99,
+        'photo' => 'ori'
+    ),
+);
+
+
+$action = array(
+    01 => array(
+        'nom' => 'ARK Survival Evolved',
+        'catégorie' => 'action',
+        'prix' => 34.99,
+        'photo' => 'ark.jpg'
+    ),
+    02 => array(
+        'nom' => 'Dead Island Riptide',
+        'catégorie' => 'action',
+        'prix' => 39.99,
+        'photo' => 'riptide.jpg'
+    ),
+    03 => array(
+        'nom' => 'Doom',
+        'catégorie' => 'action',
+        'prix' => 45.99,
+        'photo' => 'doom.jpg'
+    ),
+    04 => array(
+        'nom' => 'Assassin\'s Creed',
+        'catégorie' => 'action',
+        'prix' => 38.99,
+        'photo' => 'assassins.jpg'
+    ),
+    05 => array(
+        'nom' => 'Halo 5',
+        'catégorie' => 'action',
+        'prix' => 59.99,
+        'photo' => 'halo5.png'
+    ),
+);
+
+$reflexion = array(
+    01 => array(
+        'nom' => 'The Park',
+        'catégorie' => 'reflexion',
+        'prix' => 12.99,
+        'photo' => 'park.png'
+    ),
+    02 => array(
+        'nom' => 'Enigmatis Ghosts of Maple Creek',
+        'catégorie' => 'reflexion',
+        'prix' => 9.99,
+        'photo' => 'enigmatics.png'
+    ),
+    03 => array(
+        'nom' => 'Tetrobot',
+        'catégorie' => 'reflexion',
+        'prix' => 7.99,
+        'photo' => 'tetrobot.png'
+    ),
+    04 => array(
+        'nom' => 'ABC Murders',
+        'catégorie' => 'reflexion',
+        'prix' => 39.99,
+        'photo' => 'abcmurders.png'
+    ),
+    05 => array(
+        'nom' => 'Puzzle Bundle',
+        'catégorie' => 'reflexion',
+        'prix' => 14.99,
+        'photo' => 'puzzle.png'
+    ),
+);
+?>
