56ed9961b53e5e749fa7b3a05be0746d7bb3dc60
lduisit <lancelot.duisit@gmail.com>
2016-05-16 19:43:17 -0400

    catalogue s'affiche proprement reste à faire les liens vers produits et catégories reste à finir formulaire

--- catalogue.php
+++ catalogue.php
@@ -1,20 +1,73 @@
 <?php
 include('data/data.php');
-?>
-<?php include ('views/page_top.php'); ?>
-<div id="main">
-    <!--Code html spécifique -->
-    <?php /*Affichage du catalogue*/ ?>
-    <ul>
-        <?php foreach ($data as $id => $item) { ?>
-            <li>
-                <div>
-                    <p><?= $item['nom'] ?>, <span class=".prix"><?= $item['prix'] ?> Fifa 2016 est un jeu de soccer très populaire pour les amateurs de sport </span></p>
-                    <img src="<?= $item['photo'] ?>" alt=""/>
-                </div>
-            </li>
-        <?php } ?>
-    </ul>
-</div>
-<?php include ('views/page_bottom.php'); ?>
+//include ('views/catalogue.php');
 
+?>
+
+<!-- ----- -------- DISPLAY -------- ----- -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style/catalogue_css.css">
+    <meta charset="UTF-8">
+    <title>Catalogue</title>
+</head>
+<body>
+    <div id="header">
+        <?php require_once ('views/page_top.php'); ?>
+    </div>
+
+    <div id="catalogue">
+        <!--affichage catalogue -->
+        <?php
+        //fonction d'appel d'images et infos
+        function affiche_image($texte_alt, $image_path)
+        {
+            return '<img src="' . $image_path . '" alt="' . $texte_alt . '">';
+        }
+
+        echo '<h2>SPORTS</h2>';
+        //boucles d'appel de fonction
+        foreach ($sports as $clef => $valeur) {
+            echo '<p>';
+            echo '<a href="/">', $valeur['nom'], '</a><br>';
+            echo '<a href="/">','$', $valeur['prix'], '</a><br>';
+            echo '<a href="/">', affiche_image('photo', $valeur['img_path']), '</a><br>'; // Revenir là-dessus
+            echo '</p>';
+        }
+
+        echo '<h2>JEUNESSE</h2>';
+        foreach ($jeunesse as $clef => $valeur) {
+            echo '<p>';
+            echo '<a href="/">', $valeur['nom'], '</a><br>';
+            echo '<a href="/">','$', $valeur['prix'], '</a><br>';
+            echo '<a href="/">', affiche_image('photo', $valeur['img_path']), '</a><br>'; // Revenir là-dessus
+            echo '</p>';
+        }
+
+        echo '<h2>ACTION</h2>';
+        foreach ($action as $clef => $valeur) {
+            echo '<p>';
+            echo '<a href="/">', $valeur['nom'], '</a><br>';
+            echo '<a href="/">','$', $valeur['prix'], '</a><br>';
+            echo '<a href="/">', affiche_image('photo', $valeur['img_path']), '</a><br>'; // Revenir là-dessus
+            echo '</p>';
+        }
+
+        echo '<h2>RÉFLEXION</h2>';
+        foreach ($reflexion as $clef => $valeur) {
+            echo '<p>';
+            echo '<a href="/">', $valeur['nom'], '</a><br>';
+            echo '<a href="/">','$', $valeur['prix'], '</a><br>';
+            echo '<a href="/">', affiche_image('photo', $valeur['img_path']), '</a><br>'; // Revenir là-dessus
+            echo '</p>';
+        }
+        ?>
+    </div>
+
+    <div id="footer">
+        <?php require_once ('views/page_bottom.php'); ?>
+    </div>
+</body>
+</html>
--- data/data.php
+++ data/data.php
@@ -1,135 +1,156 @@
 <?php
 
-$sports = array(
-    01 => array(
-        'nom' => 'Fifa 2016',
-        'catégorie' => 'sports',
-        'prix' => 39.99,
-        'photo' => 'fifa.jpg'
-    ),
-    02 => array(
-        'nom' => 'NBA 2k 16',
-        'catégorie' => 'sports',
-        'prix' => 54.99,
-        'photo' => 'nba.jpg'
-    ),
-    03 => array(
-        'nom' => 'Madden NFL 2016',
-        'catégorie' => 'nfl.jpg',
-        'prix' => 49.99,
-        'photo' => 'photo'
-    ),
-    04 => array(
-        'nom' => 'UFC',
-        'catégorie' => 'sports',
-        'prix' => 34.99,
-        'photo' => 'ufc.jpg'
-    ),
-    05 => array(
-        'nom' => 'Infinity Air',
-        'catégorie' => 'sports',
-        'prix' => 'coming soon',
-        'photo' => 'infinite.jpeg'  // attention à l'extension ici
-    ),
-);
+$jeux = array(
+    $sports = array(
+        01 => array(
+            'nom' => 'Fifa 2016',
+            'catégorie' => 'sports',
+            'prix' => 39.99,
+            'img_path' => 'images/fifa.jpg',
+            'img_alt' => 'Image Fifa',
+            ),
+        02 => array(
+            'nom' => 'NBA 2k 16',
+            'catégorie' => 'sports',
+            'prix' => 54.99,
+            'img_path' => 'images/nba.jpg',
+            'img_alt' => 'Image NBA',
+            ),
+        03 => array(
+            'nom' => 'Madden NFL 2016',
+            'catégorie' => 'sports',
+            'prix' => 49.99,
+            'img_path' => 'images/nfl.jpg',
+            'img_alt' => 'Image NFL',
+            ),
+        04 => array(
+            'nom' => 'UFC',
+            'catégorie' => 'sports',
+            'prix' => 34.99,
+            'img_path' => 'images/ufc.jpg',
+            'img_alt' => 'Image UFC',
+            ),
+        05 => array(
+            'nom' => 'Infinity Air',
+            'catégorie' => 'sports',
+            'prix' => 'coming soon',
+            'img_path' => 'images/infinite.jpg',
+            'img_alt' => 'Image Infinity',
+            )
+        ),
 
-$jeunes = array(
-    01 => array(
-        'nom' => 'Tortues Ninja',
-        'catégorie' => 'jeunesse',
-        'prix' => 49.99,
-        'photo' => 'TNMT.jpg'
+    $jeunesse = array(
+        01 => array(
+            'nom' => 'Tortues Ninja',
+            'catégorie' => 'jeunesse',
+            'prix' => 49.99,
+            'img_path' => 'images/tnmt.jpg',
+            'img_alt' => 'Image TNMT',
+            ),
+        02 => array(
+            'nom' => 'Mystery Castle',
+            'catégorie' => 'jeunesse',
+            'prix' => 9.99,
+            'img_path' => 'images/mystery.jpg',
+            'img_alt' => 'Image Mystery',
+            ),
+        03 => array(
+            'nom' => 'JumpJet Rex',
+            'catégorie' => 'jeunesse',
+            'prix' => 9.99,
+            'img_path' => 'images/jumpjet.jpg',
+            'img_alt' => 'Image Jumjet',
+            ),
+        04 => array(
+            'nom' => 'Gryphon Knight Epic',
+            'catégorie' => 'jeunesse',
+            'prix' => 12.99,
+            'img_path' => 'images/gryphon.jpg',
+            'img_alt' => 'Image Gryphon',
+            ),
+        05 => array(
+            'nom' => 'Ori and the Blind Forest',
+            'catégorie' => 'jeunesse',
+            'prix' => 14.99,
+            'img_path' => 'images/ori.jpg',
+            'img_alt' => 'Image Ori',
+            )
     ),
-    02 => array(
-        'nom' => 'Mystery Castle',
-        'catégorie' => 'jeunesse',
-        'prix' => 9.99,
-        'photo' => 'mystery.png'
-    ),
-    03 => array(
-        'nom' => 'JumpJet Rex',
-        'catégorie' => 'jeunesse',
-        'prix' => 9.99,
-        'photo' => 'jumpjet.png'
-    ),
-    04 => array(
-        'nom' => 'Gryphon Knight Epic',
-        'catégorie' => 'jeunesse',
-        'prix' => 12.99,
-        'photo' => 'gryphon.png'
-    ),
-    05 => array(
-        'nom' => 'Ori ans the Blind Forest',
-        'catégorie' => 'jeunesse',
-        'prix' => 14.99,
-        'photo' => 'ori'
-    ),
-);
 
+    $action = array(
+        01 => array(
+            'nom' => 'ARK Survival Evolved',
+            'catégorie' => 'action',
+            'prix' => 34.99,
+            'img_path' => 'images/ark.jpg',
+            'img_alt' => 'Image ARK',
+            ),
+        02 => array(
+            'nom' => 'Dead Island Riptide',
+            'catégorie' => 'action',
+            'prix' => 39.99,
+            'img_path' => 'images/riptide.jpg',
+            'img_alt' => 'Image Riptide',
+            ),
+        03 => array(
+            'nom' => 'Doom',
+            'catégorie' => 'action',
+            'prix' => 45.99,
+            'img_path' => 'images/doom.jpg',
+            'img_alt' => 'Image Doom',
+            ),
+        04 => array(
+            'nom' => 'Assassin\'s Creed',
+            'catégorie' => 'action',
+            'prix' => 38.99,
+            'img_path' => 'images/assassins.jpg',
+            'img_alt' => 'Image Assassins',
+            ),
+        05 => array(
+            'nom' => 'Halo 5',
+            'catégorie' => 'action',
+            'prix' => 59.99,
+            'img_path' => 'images/halo5.jpg',
+            'img_alt' => 'Image Halo',
+            )
+    ),
 
-$action = array(
-    01 => array(
-        'nom' => 'ARK Survival Evolved',
-        'catégorie' => 'action',
-        'prix' => 34.99,
-        'photo' => 'ark.jpg'
-    ),
-    02 => array(
-        'nom' => 'Dead Island Riptide',
-        'catégorie' => 'action',
-        'prix' => 39.99,
-        'photo' => 'riptide.jpg'
-    ),
-    03 => array(
-        'nom' => 'Doom',
-        'catégorie' => 'action',
-        'prix' => 45.99,
-        'photo' => 'doom.jpg'
-    ),
-    04 => array(
-        'nom' => 'Assassin\'s Creed',
-        'catégorie' => 'action',
-        'prix' => 38.99,
-        'photo' => 'assassins.jpg'
-    ),
-    05 => array(
-        'nom' => 'Halo 5',
-        'catégorie' => 'action',
-        'prix' => 59.99,
-        'photo' => 'halo5.png'
-    ),
-);
-
-$reflexion = array(
-    01 => array(
-        'nom' => 'The Park',
-        'catégorie' => 'reflexion',
-        'prix' => 12.99,
-        'photo' => 'park.png'
-    ),
-    02 => array(
-        'nom' => 'Enigmatis Ghosts of Maple Creek',
-        'catégorie' => 'reflexion',
-        'prix' => 9.99,
-        'photo' => 'enigmatics.png'
-    ),
-    03 => array(
-        'nom' => 'Tetrobot',
-        'catégorie' => 'reflexion',
-        'prix' => 7.99,
-        'photo' => 'tetrobot.png'
-    ),
-    04 => array(
-        'nom' => 'ABC Murders',
-        'catégorie' => 'reflexion',
-        'prix' => 39.99,
-        'photo' => 'abcmurders.png'
-    ),
-    05 => array(
-        'nom' => 'Puzzle Bundle',
-        'catégorie' => 'reflexion',
-        'prix' => 14.99,
-        'photo' => 'puzzle.png'
-    ),
-);
+    $reflexion = array(
+        16 => array(
+            'nom' => 'The Park',
+            'catégorie' => 'reflexion',
+            'prix' => 12.99,
+            'img_path' => 'images/park.jpg',
+            'img_alt' => 'Image Park',
+            ),
+        17 => array(
+            'nom' => 'Enigmatis Ghosts of Maple Creek',
+            'catégorie' => 'reflexion',
+            'prix' => 9.99,
+            'img_path' => 'images/enigmatis.jpg',
+            'img_alt' => 'Image Enigmatis',
+            ),
+        18 => array(
+            'nom' => 'Tetrobot',
+            'catégorie' => 'reflexion',
+            'prix' => 7.99,
+            'img_path' => 'images/tetrobot.jpg',
+            'img_alt' => 'Image Tetrobot',
+            ),
+        19 => array(
+            'nom' => 'ABC Murders',
+            'catégorie' => 'reflexion',
+            'prix' => 39.99,
+            'img_path' => 'images/abcmurders.jpg',
+            'img_alt' => 'Image ABC',
+            ),
+        20 => array(
+            'nom' => 'Puzzle Bundle',
+            'catégorie' => 'reflexion',
+            'prix' => 14.99,
+            'img_path' => 'images/puzzle.jpg',
+            'img_alt' => 'Image Puzzle',
+            )
+    )
+)
 ?>
