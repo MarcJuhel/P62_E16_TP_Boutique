7818c25c0bbbd37177665520e615a23fda8ed302
juhelmarc <juhelmarc@gmail.com>
2016-05-16 11:22:58 -0400

    plus de css

--- views/page_top.php
+++ views/page_top.php
@@ -15,23 +15,24 @@
     echo '<a href="views/form_achat.php" class="xone">Xbox One</p> ';
     echo '<a href="views/form_achat.php" class="x360">Xbox 360</p>';
     ?>
-    <h1>Meilleures ventes</h1>
+
+</div>
+<div id="meilleuresventes"
+<h1>Meilleures ventes</h1>
 </div>
 
 <div id ="imagetop">
-
     <?php
-    echo '<a href="views/form_achat.php"><img src="images/fifa.jpg"alt="fifa"><p>39.99</p><p>Achetez</p></a>';
-    echo '<a href="views/form_achat.php"><img src="images/assassins.jpg"alt="assassins"><p>38.99</p><p>Achetez</p>';
-    echo '<a href="views/form_achat.php"><img src="images/doom.jpg"alt="doom"><p>45.99</p><p>Achetez</p>';
-    echo '<a href="views/form_achat.php"><img src="images/riptide.jpg"alt="riptide"><p>39.99</p><p>Achetez</p>';
-    echo '<a href="views/form_achat.php"><img src="images/nfl.jpg"alt="nfl"><p>49.99</p><p>Achetez</p>';
-    echo '<a href="views/form_achat.php"><img src="images/ark.jpg"alt="ark"><p>34.99</p><p>Achetez</p>';
-    echo '<a href="views/form_achat.php"><img src="images/recycled-best-sellers_FR.jpg"alt="best">';
-    echo '<a href="https://www.youtube.com/user/GameStopCanada"><img src="images/youtube-subscribe_white_FR.jpg"alt="youtube">';
+    echo '<a href="views/form_achat.php" class="imagetop"><img src="images/fifa.jpg"alt="fifa"><p>39.99</p><p>Achetez</p></a>';
+    echo '<a href="views/form_achat.php"  class="imagetop"><img src="images/assassins.jpg"alt="assassins"><p>38.99</p><p>Achetez</p>';
+    echo '<a href="views/form_achat.php" class="imagetop"><img src="images/doom.jpg"alt="doom"><p>45.99</p><p>Achetez</p>';
+    echo '<a href="views/form_achat.php" class="imagetop"><img src="images/riptide.jpg"alt="riptide"><p>39.99</p><p>Achetez</p>';
+    echo '<a href="views/form_achat.php" class="imagetop"><img src="images/nfl.jpg"alt="nfl"><p>49.99</p><p>Achetez</p>';
+    echo '<a href="views/form_achat.php" class="imagetop"><img src="images/ark.jpg"alt="ark"><p>34.99</p><p>Achetez</p>';
+    echo '<a href="views/form_achat.php" class="imagetop"><img src="images/recycled-best-sellers_FR.jpg"alt="best">';
+    echo '<a href="https://www.youtube.com/user/GameStopCanada" class="imagetop"><img src="images/youtube-subscribe_white_FR.jpg"alt="youtube">';
     ?>
 </div>
-
 <h1>Nouveaux et a venir</h1>
 
 <div id ="nouveaute">
